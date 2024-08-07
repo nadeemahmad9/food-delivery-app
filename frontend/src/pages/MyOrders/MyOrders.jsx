584e72433119969256ebe3a9613a6a61a9d983c7
nadeemahmad9 <127886628+nadeemahmad9@users.noreply.github.com>
2024-08-07 18:51:10 +0530

    Update MyOrders.jsx

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -36,7 +36,7 @@ const MyOrders = () => {
                                 return item.name+" x "+item.quantity+","
                             }
                         })}</p>
-                        <p>${order.amount}.00</p>
+                        <p>₹{order.amount}.00</p>
                         <p>Items: {order.items.length}</p>
                         <p><span>&#x25cf;</span> <b>{order.status}</b></p>
                         <button onClick={fetchOrders}>Track Order</button>
