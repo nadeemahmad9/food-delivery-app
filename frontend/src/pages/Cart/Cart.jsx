bce5c296ba925104bb83ce273023947eb7d3dc95
nadeemahmad9 <127886628+nadeemahmad9@users.noreply.github.com>
2024-08-07 18:50:05 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -30,9 +30,9 @@ const Cart = () => {
                                 <div className='cart-items-title cart-items-item'>
                                     <img src={url + "/images/" + item.image} alt="" />
                                     <p>{item.name}</p>
-                                    <p>${item.price}</p>
+                                    <p>₹{item.price}</p>
                                     <p>{cartItems[item._id]}</p>
-                                    <p>${item.price * cartItems[item._id]}</p>
+                                    <p>₹{item.price * cartItems[item._id]}</p>
                                     <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
                                 </div>
                                 <hr />
@@ -47,17 +47,17 @@ const Cart = () => {
                     <div>
                         <div className="cart-total-details">
                             <p>Subtotal</p>
-                            <p>${getTotalCartAmount()}</p>
+                            <p>₹{getTotalCartAmount()}</p>
                         </div>
                         <hr />
                         <div className="cart-total-details">
                             <p>Delivery Fee</p>
-                            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+                            <p>₹{getTotalCartAmount() === 0 ? 0 : 2}</p>
                         </div>
                         <hr />
                         <div className="cart-total-details">
                             <b>Total</b>
-                            <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+                            <b>₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
                         </div>
                     </div>
                     <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
