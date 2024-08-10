12d550ed4f2d1306f38107bfe57cce88f86a78c5
nadeemahmad9 <127886628+nadeemahmad9@users.noreply.github.com>
2024-08-10 18:38:13 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -61,7 +61,7 @@ import profile_icon from './profile_icon.png'
 import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
-import banner3 from '.banner3.jpg'
+import banner3 from './banner3.jpg'
 
 export const assets = {
     logo,
