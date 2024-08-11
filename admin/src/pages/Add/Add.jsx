6203c8d17407cdf7d0ea549d97aa283e8786f3f8
nadeemahmad9 <127886628+nadeemahmad9@users.noreply.github.com>
2024-08-11 20:52:17 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -76,6 +76,8 @@ const Add = ({ url }) => {
                             <option value="Pure Veg">Pure Veg</option>
                             <option value="Pasta">Pasta</option>
                             <option value="Noodles">Noodles</option>
+                            <option value="Pizza">Pizza</option>
+
                         </select>
                     </div>
                     <div className="add-price flex-col">
