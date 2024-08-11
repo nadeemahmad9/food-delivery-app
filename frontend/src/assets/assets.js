8d83b1bf51534abe40b616941f45cf8aab9f1fc2
nadeemahmad9 <127886628+nadeemahmad9@users.noreply.github.com>
2024-08-11 20:48:52 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -12,7 +12,7 @@ import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
 import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
-import menu_9 from './menu_9.png
+import menu_9 from './menu_9.png'
 
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
