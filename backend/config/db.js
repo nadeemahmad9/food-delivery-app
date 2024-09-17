e3fc61359be186970b4d804ba50dc6fef899b4e5
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2024-09-17 12:50:35 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://thebatmann:<db_password>@cluster0.immmjnb.mongodb.net/').then(() => console.log("DataBase Connected"));
+    await mongoose.connect('mongodb+srv://thebatmann:pass654321@cluster0.immmjnb.mongodb.net/food-delivery').then(() => console.log("DataBase Connected"));
 }
